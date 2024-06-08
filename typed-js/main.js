2da33a45a53745e158e0776099e5f2e45efcd539
mitragroups <miftahmuhajir@gmail.com>
2024-06-09 06:15:19 +0700

    Add typed-js

---
+++ typed-js/main.js
@@ -0,0 +1,7 @@
+const Typed = require("typed.js");
+
+const typed = new Typed("#heading", {
+  strings: ["Hello, I'm Mamu", "I'm a Toodler", "I love JavaScript for Kids"],
+  typeSpeed: 50,
+  loop: true,
+});
